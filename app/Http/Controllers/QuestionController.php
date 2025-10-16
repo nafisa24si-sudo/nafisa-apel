dc77af71c42c8aef2a39b8f70ac5e22c92080262
Nafisa-Apel <nafisa24si@mahasiswa.pcr.ac.id>
2025-10-16 11:24:41 +0700

    pertemuan 6 nafisa

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'nama'       => 'required|max:10',
+            'email'      => ['required', 'email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ], [
+
+            'nama.required' => 'Nama wajib diisi',
+            'email.email'   => 'Format email tidak valid',
+
+        ]);
+
+        $data['nama']       = $request->nama;
+        $data['email']      = $request->email;
+        $data['pertanyaan'] = $request->pertanyaan;
+
+        //return view('home-question-respon', $data);
+        // return view('home-question-respon', $data);
+        return redirect()->route('home')->with('info', 'Terimakasih <b>' . $data['nama'] .
+            '</b> Data pertanyaan berhasil di inputkan, Silahkan cek email <b>'
+            . $data['email'] . '</b>');
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\dashboardController;
+// use App\Http\Controllers\DashboardController;
+// use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,11 +19,22 @@ Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
 })->name('mahasiswa.show');
 
-Route::get('/nama/{param1?}', function ($param1='') {
-    return 'Nama saya: '.$param1;
+Route::get('/nama/{param1?}', function ($param1 = '') {
+    return 'Nama saya: ' . $param1;
 });
 Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+
+Route::post('question/store', [QuestionController::class, 'store'])
+    ->name('question.store');
+
+Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
+    ->name('dashboard');
+
+    Route::resource('pelanggan', PelangganController::class);
